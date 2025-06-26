e98cd19dad15dd8010fcee1424107ab04570f77a
nadun96 <nadunudaraka@gmail.com>
2025-06-26 18:04:52 +0530

    added new schema

--- src/index.js
+++ src/index.js
@@ -1,25 +1,29 @@
-import express from "express";
-import { graphqlHTTP } from "express-graphql";
-import schema from "../data/schema.js";
+import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import schema from '../data/schema';
+
 const PORT = 8080;
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("GraphQL is running!");
+app.get('/', (req, res) => {
+    res.send('GraphQL is amazing!');
 });
 
-const root = { hello: () => "Hi, I'm Manny!" };
+const root = { product: () => {
+    return {
+        "id": 29729872,
+        "name": "Widget",
+        "description": "Beautiful widget to use in the garden",
+        "price": 34.99,
+        "soldout": false
+    }
+}};
 
-app.use(
-  "/graphql",
-  graphqlHTTP({
+app.use('/graphql', graphqlHTTP({
     schema: schema,
     rootValue: root,
     graphiql: true,
-  })
-);
+}));
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}/graphql`);
-});
+app.listen(PORT, () => console.log(`Running server on localhost:${PORT}/graphql`));
