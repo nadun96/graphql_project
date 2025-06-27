03489f67c5ec97205f631d446b6275a0d342c0cb
nadun96 <nadunudaraka@gmail.com>
2025-06-27 13:36:58 +0530

    delete mutation

--- src/data/resolvers.js
+++ src/data/resolvers.js
@@ -51,6 +51,14 @@ const resolvers = {
             throw new Error(error);
         }
         
+    },
+    deletProduct: async ({ id }) => {
+        try {
+            await Widgets.deleteOne({ _id: id });
+            return 'Successfully deleted widget';
+        } catch (error) {
+            throw new Error(error);
+       }
     }
 }
 
